7621ba0767b1f21329bd2ea62a874396579704c0
unknown <astrutledge@gmail.com>
2020-05-04 17:04:27 -0600

    day1

--- app/conditionals.js
+++ app/conditionals.js
@@ -2,7 +2,9 @@
 
 function largerNum(num1, num2) {
     // you code here
-
+if (num1 > num2){
+    return num1}
+else { return num2}
 }
 
 // 2. Given two numbers, amount correct and amount possible of a test, return the corresponding letter grade.
@@ -14,8 +16,23 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
-
+    let x = score/possible;
+if (x >= .90){
+    return "A"
 }
+ else if (x >= .80){
+    return "B"
+}
+else if (x >= .70){
+    return "C"
+}
+else if (x >= .60){
+    return "D"
+}
+else {return "F"}
+}
+
+
 
 
 // --------------------------------------------
@@ -29,13 +46,31 @@ function testGrader(score, possible) {
 //Make sure your ranges are inclusive
 
 function timeOfDayGreeting(hour) {
-    // you code below
-
+    if (hour >= 22){
+        return "good night"
+    }
+    else if (hour >= 18){
+        return "good evening"
+    }
+    else if (hour >= 12){
+        return "good afternoon"
+    }
+    else if (hour >= 5){
+        return "good morning"
+    }
+    else{return "good night"}
 }
 
 //4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
 function isFever(temp) {
-
+if (temp >= 103){
+return 'fever go to hospital'
+}
+else if (temp >98.6){
+    return 'fever'
+} else {
+    return 'no fever'
+}
 }
 
 //5. Write a function that takes in a car object, if it is not moving then return true
@@ -46,8 +81,13 @@ let myCar = {
     moving: false
 }
 
-function isStopped(car) {
-
+function isStopped(myCar) {
+if (myCar.moving == false){
+    return true
+} 
+else if (myCar.moving == true) {
+    return false
+}
 }
 
 //6. Write a function that returns true if a dish is yours and is dirty, or false if one of the statements is false
